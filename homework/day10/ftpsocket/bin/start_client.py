2efcda5e1606986559f8668dd9368655a9457b80
shuke163 <shu_ke163@163.com>
2017-07-08 10:05:47 +0800

    Add ftpsocket dir

---
+++ homework/day10/ftpsocket/bin/start_client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+BASE_DIR=os.path.normpath(os.path.join(
+    os.path.abspath(__file__),
+    os.pardir,
+    os.pardir,
+))
+sys.path.append(BASE_DIR)
+
+from client.ftpclient import main
+
+if __name__ == '__main__':
+    main()
